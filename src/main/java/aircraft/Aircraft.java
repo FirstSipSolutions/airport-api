b522521d48ed49e77340b13c172c0ae23d723baf
Justin Seaward <jut_seaward@yahoo.com>
2026-06-22 13:17:56 -0230

    Aircraft: added proper imports

--- src/main/java/aircraft/Aircraft.java
+++ src/main/java/aircraft/Aircraft.java
@@ -1,6 +1,11 @@
 package aircraft;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToMany;
+import org.springframework.data.annotation.Id;
+import java.util.List;
 
 @Entity
 public class Aircraft {
