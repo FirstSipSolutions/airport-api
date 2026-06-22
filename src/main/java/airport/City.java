2a62a9e17deb15a4116ee81e659460095b5a4688
Chris <98862485+Britten66@users.noreply.github.com>
2026-06-22 10:27:09 -0300

    fixed formatting

--- src/main/java/airport/City.java
+++ src/main/java/airport/City.java
@@ -22,34 +22,47 @@ public class City {
     }
 
     public Long getId() {
+
+
         return id;
     }
 
     public void setId(Long id) {
+
+
         this.id = id;
     }
 
     public String getName() {
+
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getState() {
+
         return state;
     }
 
     public void setState(String state) {
+
         this.state = state;
     }
 
     public int getPopulation() {
+
+
         return population;
     }
 
     public void setPopulation(int population) {
+
+
         this.population = population;
     }
 }
