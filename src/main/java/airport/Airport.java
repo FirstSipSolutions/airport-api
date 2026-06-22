31cdf8e85288c546991ae7abe2734bffd0da04b2
Chris <98862485+Britten66@users.noreply.github.com>
2026-06-22 12:33:04 -0300

    uncommented airport entitiy done

---
+++ src/main/java/airport/Airport.java
@@ -0,0 +1,62 @@
+package airport;
+
+
+// first sip solutions
+// will add comments asap
+// But  here is the airport java class and entity
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+    @ManyToOne
+    private City city;
+
+    public Airport() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+}
