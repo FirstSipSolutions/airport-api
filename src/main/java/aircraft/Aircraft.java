b5e9261cf350803915efce8f843c8b3231f58c16
Justin Seaward <jut_seaward@yahoo.com>
2026-06-22 11:45:54 -0230

    Aircraft: added aircraft entity

---
+++ src/main/java/aircraft/Aircraft.java
@@ -0,0 +1,51 @@
+package aircraft;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Aircraft {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String type;
+    private String airlineName;
+    private int numberOfPassengers;
+
+//    @ManyToMany
+//    public List<Airport> airport;
+
+    public Aircraft(){
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+}
