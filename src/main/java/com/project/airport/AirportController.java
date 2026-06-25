3c2d5b1f13d32897fc1b37cfa10c24839ce5506a
Chris <98862485+Britten66@users.noreply.github.com>
2026-06-25 10:27:03 -0300

    added comments to airportController

---
+++ src/main/java/com/project/airport/AirportController.java
@@ -0,0 +1,51 @@
+/*
+ * FirstSipSolutions
+ * Author: Chris/Justin
+ * SD 15 - 2026
+ */
+
+
+package com.project.airport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/airports")
+public class AirportController {
+
+    @Autowired
+    private AirportService airportService;
+
+    @GetMapping
+    public Iterable<Airport> getAllAirports() {
+        return airportService.getAllAirports();
+    }
+
+    @GetMapping("/{id}")
+    public Airport getAirportById(@PathVariable Long id) {
+        return airportService.getAirportById(id);
+    }
+
+    @PostMapping
+    public Airport createAirport(@RequestBody Airport airport) {
+        return airportService.createAirport(airport);
+    }
+
+    @PutMapping("/{id}")
+    public Airport updateAirport(@PathVariable Long id, @RequestBody Airport airport) {
+        return airportService.updateAirport(id, airport);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAirport(@PathVariable Long id) {
+        airportService.deleteAirport(id);
+    }
+}
--- src/main/java/com/project/airport/AirportService.java
+++ src/main/java/com/project/airport/AirportService.java
@@ -6,20 +6,33 @@
 
 package com.project.airport;
 
+// imports for spring and such
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+
+
+// first part of this service class
 @Service
 public class AirportService {
 
     @Autowired
     private AirportRepository airportRepository;
 
+
+    // here the code grabs from repoistory andreturnss
+    // all airports, from the repo get taken  in
+
     public Iterable<Airport> getAllAirports() {
         return airportRepository.findAll();
     }
+
+
+
+    // looking for the aiport byu  id
+    // if it is there it will return it, if not returns null
 
     public Airport getAirportById(Long id) {
         Optional<Airport> airport = airportRepository.findById(id);
@@ -31,12 +44,22 @@ public class AirportService {
             return null;
         }
     }
-
+    // save a new aiport  and returns back the same
+    // airport via the created airport feature
+    // adding in as many comments as I can
     public Airport createAirport(Airport airport) {
 
         return airportRepository.save(airport);
     }
 
+
+    // this part will update the existing airport
+    // pulls the repo and copies new num and then saves that value
+    // this has a handler as well for reutrning null if no vlaue
+
+
+    // that null will be important as a handler to see what goes wrong, as this is not a bulletproof section of code
+    // commited in chunks and commented after the fact
     public Airport updateAirport(Long id, Airport airportDetails) {
         Optional<Airport> optionalAirport = airportRepository.findById(id);
         if (optionalAirport.isPresent()) {
@@ -56,6 +79,8 @@ public class AirportService {
         }
     }
 
+
+    // this is a simple deletion
     public void deleteAirport(Long id) {
 
         airportRepository.deleteById(id);
