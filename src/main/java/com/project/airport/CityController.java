a679252885929caf681c75e5159096da043ef700
Justin Seaward <jut_seaward@yahoo.com>
2026-06-26 16:17:50 -0230

    added imports to City Controller and Service

--- src/main/java/com/project/airport/CityController.java
+++ src/main/java/com/project/airport/CityController.java
@@ -9,6 +9,8 @@ package com.project.airport;
 // will add in some more imports for many to many
 // that is an ongoing situation
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/project/airport/CityService.java
+++ src/main/java/com/project/airport/CityService.java
@@ -8,6 +8,8 @@
 
 package com.project.airport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
