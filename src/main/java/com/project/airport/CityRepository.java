c4d92e7575bb81911f5390d04035ca05892fda21
Chris <98862485+Britten66@users.noreply.github.com>
2026-06-26 15:15:51 -0300

    fixed another comment line in repoisitory for city

--- src/main/java/com/project/airport/CityRepository.java
+++ src/main/java/com/project/airport/CityRepository.java
@@ -13,6 +13,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.CrudRepository;
 
+
+
+//Pagination was added to the City endpoint because cities are reference data that grows over time
+
+
+
 // CrudRepository provides save .. find deklete and a find all
 // PagingAndSortingRepository adds findAll(Pageable object that is made) which SHOULD return that Page.
 // Extending both keeps normal CRUD methods and adds paging support.
